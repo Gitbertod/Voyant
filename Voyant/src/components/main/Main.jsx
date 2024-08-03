8608b14f06d3b97408feee42ef6deebd7ba846bb
Gilberto Díaz <diazp.gilberto@gmail.com>
2024-08-03 14:47:50 -0500

    ajustando video principal de Landing

--- Voyant/src/components/main/Main.jsx
+++ Voyant/src/components/main/Main.jsx
@@ -2,11 +2,9 @@ import bgvideo2 from "../../assets/videobg.mov";
 import styles from "./Main.module.css";
 const Main = () => {
   return (
-    <div className="main">
-      
+    <div className={styles.main}>
         <div className="overlay"></div>
-        <video src={bgvideo2} autoPlay loop muted></video>
-      
+        <video src={bgvideo2} autoPlay loop muted className={styles.video}></video>
     </div>
   );
 };
