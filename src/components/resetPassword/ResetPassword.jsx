a4f11d0323a969aa6b49b3ad433beb8cbe8827ec
Gilberto Díaz <diazp.gilberto@gmail.com>
2025-10-15 17:25:04 -0500

    se mejoraron estilos de form ResetPassword

--- src/components/resetPassword/ResetPassword.jsx
+++ src/components/resetPassword/ResetPassword.jsx
@@ -29,7 +29,12 @@ const ResetPassword = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-80">
+      <form onSubmit={handleSubmit} className="w-80 bg-gradient-to-b from-blue-950 via-blue-750 to-blue-900 shadow-lg rounded-lg p-6 flex flex-col">
+        <img
+            src="/SomosVoyant.png"
+            alt="somos voyant logo"
+            className="w-32 mx-auto mb-6 object-contain"
+          />
         <h2 className="text-xl font-bold mb-4 text-center">Restablecer contraseña</h2>
         <input
           type="password"
@@ -43,7 +48,7 @@ const ResetPassword = () => {
           className="border p-2 w-full mb-3"
           onChange={(e) => setPasswordConfirm(e.target.value)}
         />
-        <button className="bg-blue-600 text-white py-2 rounded w-full">Guardar nueva contraseña</button>
+        <button className="bg-yellow-400 hover:bg-yellow-500 transition-colors w-full text-white py-2 rounded">Guardar nueva contraseña</button>
         {message && <p className="mt-3 text-center text-sm">{message}</p>}
       </form>
     </div>
